374c5c45fb647759f482d2f45268eb311ab7a3eb
MathBriton <mathhbrito@gmail.com>
2025-02-25 18:08:05 -0300

    Models/ProductApi

--- src/VShop.ProductApi/obj/Debug/net9.0/VShop.ProductApi.AssemblyInfo.cs
+++ src/VShop.ProductApi/obj/Debug/net9.0/VShop.ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VShop.ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f51273a78c09a9f6cdf4b88ef239438b4da2cd44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb806f7b04b5ce823d279564d15df10f5352dac")]
 [assembly: System.Reflection.AssemblyProductAttribute("VShop.ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VShop.ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
